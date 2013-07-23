e36b14ce6b1f4144111d47229ecfe06230472252
George Wong <georgewong93@gmail.com>
2013-07-23 17:53:15 -0400

    Updated redis service

--- redis/datacollector.js
+++ redis/datacollector.js
@@ -8,164 +8,149 @@
  *
  */
 
-
 /* Include relevant libraries */
-var zmq = require('zmq')
+var zmq = require('zmq');
 var sock = zmq.socket('pull');
 var redis = require("redis");
 var ProtoBuf = require('protobufjs');
+var Long = require("long");
+var count = 0;
+
 
 /* Connect to redis server */
 client = redis.createClient();
-sock.connect('tcp://127.0.0.1:7200');
+sock.connect('tcp://127.0.0.1:7211');
 console.log('\033[00;31mWorker connected to port 7200\033[00m');
 
 /* Build communication protocol */
-var Packet = ProtoBuf.protoFromFile("packet.proto").build("Packet");
+var Packet = ProtoBuf.protoFromFile("packet.proto_new").build("PB_MSG.Packet");
 
 /* Variable inits and defns */
 var database = new Array();
 var time = 0;
-var previous = new Array(82);
+var previous = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
-
-
+// Creates the Cron, which creates a new folder, pushes the data and the pushes the keys every minute
 var cronJob = require('cron').CronJob;
 var sys = require('sys');
 var exec = require('child_process').exec;
-
 new cronJob('0 * * * * *', function(){
+  client.save();
+  var ts = new Date().getTime();
+  var fileN = ts + "d.rdb";
+  console.log(fileN);
 
-client.save();
+  var cmd = "cp /opt/tools/redis/redis-2.6.14/src/mydump.rdb /opt/tools/redis/redis-2.6.14/src/snapshots/" + fileN;
 
-var ts = new Date().getTime();
-var fileN = ts + "d.rdb"
-console.log(fileN);
+  console.log("command   " + cmd);
 
-var cmd = "cp /opt/tools/redis/redis-2.6.14/src/mydump.rdb /opt/tools/redis/redis-2.6.14/src/snapshots/" + f
+  var child = exec(cmd, function(error, stdout, stderr) { });
+  var cmd2 = "rm /opt/tools/redis/redis-2.6.14/src/mydump.rdb ";
+  var child2 = exec(cmd2, function(error, stdout, stderr) { });
 
-console.log("command   " + cmd);
+  client.keys("*", function (err, keys) {
+    keys.forEach(function(key , pos) {
+      client.migrate("localhost", 6381, key, 0, 1000);
+    });
+  });
 
-var child = exec(cmd, function(error, stdout, stderr)
-{
-
-});
-
-var cmd2 = "rm /opt/tools/redis/redis-2.6.14/src/mydump.rdb "
-
-var child2 = exec(cmd2, function(error, stdout, stderr)
-{
-
-});
-
-client.keys("*", function (err, keys) {
-
-keys.forEach(function(key , pos)
-{
-
-client.migrate("localhost", 6381, key, 0, 1000);
-
-});
-
-});
-
-
-console.log(ts);
-
+  console.log(ts);
 }, null, true);
 
-for(var i in previous)
-{
-previous[i] = 0;
-console.log(previous[i][0]);
-}
-
-
-sock.on('message', function(msg){
+// Takes in the data from the c++ zmq server
+sock.on('message', function(msg) {
   var message = Packet.decode(msg);
-
-  time = message.time;  
-  console.log(message.time);
-  database.push(message.time);
-  client.hmset("" + message.time, "Sys ticks", "" + message.ticks);
-  
+  time = message.time["low"];  
+  database.push(message.time["low"]);
+ 
   var i = 0;
+  var v1 = new Long(message.net[0]["obytes64_2"], message.net[0]["obytes64_1"]).toNumber();
+  var v2 = new Long(message.net[0]["rbytes64_2"], message.net[0]["rbytes64_1"]).toNumber();
+  var v3 = new Long(message.net[0]["opackets_2"], message.net[0]["opackets_1"]).toNumber();
+  var v4 = new Long(message.net[0]["ipackets_2"], message.net[0]["ipackets_1"]).toNumber();
 
-  while(typeof(message.process[i]) != "undefined") {
-    client.hmset("" + message.time, "Pro PID " + i, "" + message.process[i]["pid"]);
-    client.hmset("" + message.time, "Pro execname " + i, "" + message.process[i]["execname"]);
-    client.hmset("" + message.time, "Pro Usage " + i, "" + message.process[i]["usage"]);
-    client.hmset("" + message.time, "Pro CPU " + i, "" + message.process[i]["cpu"]);
-    i++;
+  console.log(count);
+
+//Some data requires subtraction from previous data, so the first data set is not printed
+  if(count > 0); {
+    while(typeof(message.process[i]) != "undefined") {
+      client.hmset("" + time, "Pro PID " + i, "" + message.process[i]["pid"]);
+      client.hmset("" + time, "Pro execname " + i, "" + message.process[i]["execname"]);
+      client.hmset("" + time, "Pro Usage " + i, "" + message.process[i]["usage"]);
+      client.hmset("" + time, "Pro CPU " + i, "" + message.process[i]["cpu"]);
+      i++;
+    }
+
+    i = 0;
+    while(typeof(message.cpu[i]) != "undefined") {
+      client.hmset("" + time, "CPU core " + i, "" + message.cpu[i]["core"]);
+      client.hmset("" + time, "CPU usage " + i, "" + message.cpu[i]["usage"]);
+      i++;
+    }
+
+    client.hmset("" + time, "Sys ticks", "" + message.ticks);
+    client.hmset("" + time, "Mem physmem", "" + new Long(message.mem[0]["physmem_2"], message.mem[0]["physmem_1"]).toNumber());
+    client.hmset("" + time, "Mem rss", "" + new Long(message.mem[0]["rss_2"], message.mem[0]["rss_1"]).toNumber());
+    client.hmset("" + time, "Mem pp_kernel", "" + new Long(message.mem[0]["pp_kernel_2"], message.mem[0]["pp_kernel_1"]).toNumber());
+    client.hmset("" + time, "Mem physcap", "" + new Long(message.mem[0]["physcap_2"], message.mem[0]["physcap_1"]).toNumber());
+    client.hmset("" + time, "Mem freemem", "" + new Long(message.mem[0]["freemem_2"], message.mem[0]["freemem_1"]).toNumber());
+    client.hmset("" + time, "Mem swap", "" + new Long(message.mem[0]["swap_2"], message.mem[0]["swap_1"]).toNumber());
+    client.hmset("" + time, "Mem scap", "" + new Long(message.mem[0]["swapcap_2"], message.mem[0]["swapcap_1"]).toNumber());
+
+    client.hmset("" + time, "Net obytes64", "" + (v1 - previous[0]));
+    client.hmset("" + time, "Net rbytes64", "" + (v2 - previous[1]));
+    client.hmset("" + time, "Net opackets", "" + (v3 - previous [2]));
+    client.hmset("" + time, "Net ipackets", "" + (v4 - previous [3]));
+    client.hmset("" + time, "Net instance", "" + message.net[0]["instance"]);
+
+    i = 0;
+    while(typeof(message.callheat[i]) != "undefined") {
+      client.hmset("" + time, "CallHeat name " + i, "" + message.callheat[i]["name"]);
+      client.hmset("" + time, "CallHeat lowt " + i, "" + new Long(message.callheat[i]["lowt_2"], message.callheat[i]["lowt_1"]).toNumber());
+      client.hmset("" + time, "CallHeat value " + i, "" + new Long(message.callheat[i]["value_2"], message.callheat[i]["value_1"]).toNumber());
+      i++;
+    }
   }
-
-  i = 0;
-  while(typeof(message.cpu[i]) != "undefined") {
-    client.hmset("" + message.time, "CPU core " + i, "" + message.cpu[i]["core"]);
-    client.hmset("" + message.time, "CPU usage " + i, "" + message.cpu[i]["usage"]);
-    i++;
-  }
-
-    client.hmset("" + message.time, "Mem physmem", "" + message.mem["physmem"]);
-    client.hmset("" + message.time, "Mem rss", "" + message.mem["rss"]);
-    client.hmset("" + message.time, "Mem pp_kernel", "" + message.mem["pp_kernel"]);
-    client.hmset("" + message.time, "Mem physcap", "" + message.mem["physcap"]);
-    client.hmset("" + message.time, "Mem freemem", "" + message.mem["freemem"]);
-    client.hmset("" + message.time, "Mem swap", "" + message.mem["swap"]);
-    client.hmset("" + message.time, "Mem scap", "" + message.mem["scap"]);
-
-    client.hmset("" + message.time, "Net obytes64", "" + (message.net[0]["obytes64"] - previous[0]));
-    previous[0] = message.net[0]["obytes64"];
-    client.hmset("" + message.time, "Net rbytes64", "" + (message.net[0]["rbytes64"] - previous[1]));
-    previous[1] = message.net[0]["rbytes64"];
-    client.hmset("" + message.time, "Net opackets", "" + (message.net[0]["opackets"] - previous [2]));
-    previous[2] = message.net[0]["opackets"];
-    client.hmset("" + message.time, "Net ipackets", "" + (message.net[0]["ipackets"] - previous [3]));
-    previous[3] = message.net[0]["ipackets"];
-    client.hmset("" + message.time, "Net instance", "" + message.net[0]["instance"]);
-
+    
+  previous[0] = v1;
+  previous[1] = v2;
+  previous[2] = v3;
+  previous[3] = v4;
+   
   i = 0;
   while(typeof(message.disk[i]) != "undefined") {
-    client.hmset("" + message.time, "Dis instance " + i, "" + message.disk[i]["instance"]);
-    client.hmset("" + message.time, "Dis nread " + i, "" + (message.disk[i]["nread"] - previous[4*(i+1)]));i
-    previous[4*(i+1) + 0] = message.disk[i]["nread"];
-    client.hmset("" + message.time, "Dis nwritten " + i, "" + (message.disk[i]["nwritten"] - previous[4*(i+1) + 1]));
-    previous[4*(i+1) + 1] = message.disk[i]["nwritten"];
-    client.hmset("" + message.time, "Dis reads " + i, "" + (message.disk[i]["reads"] - previous[4*(i+1) + 2]));
-    previous[4*(i+1) + 2] = message.disk[i]["reads"];
-    client.hmset("" + message.time, "Dis writes " + i, "" + (message.disk[i]["writes"] - previous[4*(i+1) + 3]));
-    previous[4*(i+1) + 3] = message.disk[i]["writes"];
-    client.hmset("" + message.time, "Dis wtime " + i, "" + (message.disk[i]["wtime"] - previous[4*(i+1) + 4]));
-    previous[4*(i+1) + 4] = message.disk[i]["wtime"];
-    client.hmset("" + message.time, "Dis wlentime " + i, "" + (message.disk[i]["wlentime"] - previous[4*(i+1) + 5]));
-    previous[4*(i+1) + 5] = message.disk[i]["wlentime"];
-    i++;
-  }
+    client.hmset("" + time, "Dis instance " + i, "" + message.disk[i]["instance"]);
   
-  i = 0;
-  while(typeof(message.callheat[i]) != "undefined") {
-    client.hmset("" + message.time, "CallHeat name " + i, "" + message.callheat[i]["name"]);
-    client.hmset("" + message.time, "CallHeat lowt " + i, "" + message.callheat[i]["lowt"]);
-    client.hmset("" + message.time, "CallHeat hight " + i, "" + message.callheat[i]["hight"]);
-    client.hmset("" + message.time, "CallHeat value " + i, "" + message.callheat[i]["value"]);
+    var d1 = new Long(message.disk[i]["nread_2"], message.disk[i]["nread_1"]).toNumber();
+    var d2 = new Long(message.disk[i]["nwritten_2"], message.disk[i]["nwritten_1"]).toNumber();
+    var d3 = message.disk[i]["reads"];
+    var d4 = message.disk[i]["writes"];
+    var d5 = new Long(message.disk[i]["wtime_2"], message.disk[i]["wtime_1"]).toNumber();
+    var d6 = new Long(message.disk[i]["wlentime_2"], message.disk[i]["wlentime_1"]).toNumber();
+   
+    if(count > 0) {
+      client.hmset("" + time, "Dis nread " + i, "" + (d1 - previous[6*(i) + 4]));
+      client.hmset("" + time, "Dis nwritten " + i, "" + (d2 - previous[6*(i) + 5]));
+      client.hmset("" + time, "Dis reads " + i, "" + (d3 - previous[6*(i) + 6]));
+      client.hmset("" + time, "Dis writes " + i, "" + (d4 - previous[6*(i) + 7]));
+      client.hmset("" + time, "Dis wtime " + i, "" + (d5 - previous[6*(i) + 8]));
+      client.hmset("" + time, "Dis wlentime " + i, "" + (d6 - previous[6*(i) + 9]));
+      } 
+ 
+    previous[6*(i) + 4] = d1;
+    previous[6*(i) + 5] = d2;
+    previous[6*(i) + 6] = d3;
+    previous[6*(i) + 7] = d4;
+    previous[6*(i) + 8] = d5;
+    previous[6*(i) + 9] = d6;
     i++;
-  }
-
+  }  
+  count++;
 });
 
 /* Handle errors */
 client.on("error", function (err) {
   console.log("Error " + err);
 });
-
-/* Print out logged entries every 673 ms 
-setInterval(function() {
-  for(var i in database) {
-    client.hgetall("" + database[i], function(err,res) {
-            
-            console.log(res);
-
-            });    
-}
-  
-}, 673);*/
--- redis/dataquery.js
+++ redis/dataquery.js
@@ -1,134 +1,197 @@
 // Sending commands in response to other commands.
 // This example runs "type" against every key in the database
-//
+
+// Edit the config Redis File to match the folder that is going to be queried
+var fs = require('fs');
+
+var a = fs.readdirSync('/opt/tools/redis/redis-2.6.14/src/snapshots');
+  fs.open('/opt/tools/redis/redis-2.6.14/redis.conf4', 'a', function(err, fd) { 
+  fs.write(fd, a[2], 5183, 18);
+  fs.close(fd);
+});
+
+// Starts the Redis Server
+var sys = require('sys');
+var exec = require('child_process').exec;
+var cmd = "sh /opt/tools/redis/redis-2.6.14/src/run2";
+var child = exec(cmd, function(error, stdout, stderr) { });
+
+// Creates the Redis Client and all other global variables needed
 var client = require("redis").createClient(6380);
+var util = require("util");
 var multi = new Array();
 var types = new Array();
 var time = new Array();
+var total = 0;
+var max = 0;
+var counts = 0;
+var finished = false;
 
-function appendArray(pose, res, pos)
-{
+// For the CallHeat function. Creates the chart to display the quantization
+function appendArray(pose, res, pos, size) {
 
-
-if(multi[pose] == undefined)
-{
-multi[pose] = new Array();
-}
-
-if(types[pose] == undefined)
-{
-types[pose] = pose;
-}
-
-multi[pose][pos] = res;
-
-return;
-}
-
-function getStat(time1, time2, section, stat, type, value, want)
-  {
-  
-  client.keys("*", function (err, keys) {
-
-      keys.sort(function(a,b){return a - b});
-      keys.forEach(function(key , pos)
-      {
-        console.log(key);  
-        
-        
-       if(key >= time1 && key <= time2)
-        {
-       client.hgetall(key,  function (err, res) { 
-       
-        for(var str in res)
-        {
-        if(str.substring(0,3) == section)
-            {
-            if(str.substring(4,7) == stat.substring(0,3))
-                 {
-                  
-                       if(type == 0)
-                     {
-                     if(res[str] >= value)
-                     {
-                         console.log(stat + " " + res[str]);
-                         for (var sstr in res)
-                              {
-                           if(sstr.substring(0,3) == want)
-                                {
-                                  console.log(sstr + "   " +  res[sstr]);
-                                }
-                              }
-                      } 
-                   }
-                   else
-                   {
-                   if(res[str] <= value)
-                     {
-                         console.log(stat + " " + res[str]);
-                         for (var sstr in res)
-                              {
-                           if(sstr.substring(0,3) == want)
-                                {
-                                  console.log(res[sstr]);
-                                }
-                             }
-                      }
-                    }
-                  
-                 }
-            }  
-        }  
-      
-        });
-        }
-      
-      });
-    client.quit();
-      }); 
-    
+  if (multi[pose] == undefined) {
+    multi[pose] = new Array(size);
+    for(var i=0; i<multi[pose].length; i++) {
+      multi[pose][i] = 0;
+    } 
   }
 
-function callHeat() {
+  if(types[pose] == undefined) {
+    types[pose] = pose;
+  }
 
-client.keys("*", function (err, keys) {
-
-      keys.sort(function(a,b){return a - b});
- 
-      keys.forEach(function(key , pos)
-      {
-       client.hgetall(key,  function (err, res) {
-            time.push(key);
-            var  pose = "";
-            for (var str in res)
-            {
-            if(str.substring(0,8) == "CallHeat" && str.substring(9,13) == "lowt")
-              {
-              pose = res[str];
-              }
-            
-            if(str.substring(0,8) == "CallHeat" && str.substring(9,14) == "value")
-              {
-              appendArray(Number(pose), res[str], pos);
-              pose = ""; 
-              }
-              }
-              
-            if (pos === (keys.length - 1)) { 
-              console.log("times       " + time);
-              for(var pose in types)
-              {
-              console.log(pose + " - " + (2* pose) + "             " +  multi[pose]);
-              }
-              }
-            
-            }); 
-              
-      });
-    client.quit();
-
-});
+  multi[pose][pos] = res;
+  
+  return;
 }
 
-callHeat();
-//getStat(0, 12250279085936882, "Dis", "writes", 0, 10, "Pro");
+// Returns the over/under of a specific stat, and returns a different section
+function getStat(section, stat, type, value, want) {
+
+  client.keys("*", function (err, keys) {
+    keys.sort(function(a,b){return a - b});
+    keys.forEach(function(key , pos) {
+      client.hgetall(key,  function (err, res) { 
+
+      // Goes through the key to find the specific stat 
+      for(var str in res) {
+        if(str.substring(0,3) == section) {
+          if(str.substring(4,7) == stat.substring(0,3)) {
+            //Checks to see if the stat is above or below (specified) the value
+            if(type == 0) {
+            if(res[str] >= value) {
+              counts++;
+              console.log(counts);
+              console.log(key);
+              console.log(stat + " " + res[str]);
+              for (var sstr in res) {
+                if(sstr.substring(0,3) == want) {
+                  console.log(sstr + "   " +  res[sstr]);
+                }
+              }
+            } 
+          } else {
+            if(res[str] <= value) {
+              console.log(stat + " " + res[str]);
+              for (var sstr in res) {
+                if(sstr.substring(0,3) == want) console.log(res[sstr]);
+              }
+            }
+          }
+
+          }
+        }  
+      }
+
+      //Shuts down Redis Client
+      if(pos == keys.length -1) shutDown();
+
+      });
+    });
+  });
+}
+
+// Generates Max, Average and counts for dataset
+
+function genStats(section, stat, instance) {
+  client.keys("*", function (err, keys) {
+  keys.sort(function(a,b){return a - b});   
+
+  keys.forEach(function(key , pos) {
+  client.hgetall(key,  function (err, res) {
+
+  //Searches for the specific stat               
+
+  for(var str in res) {
+    if(str.substring(0,3) == section) {
+      if(str.substring(4,7) == stat.substring(0,3)) {                        
+
+        // Add up the data
+        var number = Number(res[str]);
+  
+        if(instance == undefined) {
+          total += number;
+          if(number > max) max = number;
+          counts++;
+        } else {
+          if (str.substring(str.length - 2) == instance) {
+            total += number;
+            if(number > max) max = number;
+            counts++;
+          }  
+        }
+      }
+    }
+  }
+  // Print the totals and close the redis server
+
+  if(pos == keys.length -1) {              
+    if(instance == undefined) {
+      console.log("average " + section + " " + stat + " " + (total/counts));
+    }
+    if(instance != undefined) {
+      console.log("average " + section + " " + stat + " Instance" + instance  + ": " + (total/counts));
+    }
+    console.log("max   " + max); 
+    console.log("count " + (counts));
+    shutDown(); 
+  }
+  });      
+  });
+  });
+}
+
+function shutDown() {
+  client.shutdown();
+  client.quit();
+}
+
+//Print on quantized chart
+function callHeat() {
+  client.keys("*", function (err, keys) {
+    keys.sort(function(a,b){return a - b});
+    keys.forEach(function(key , pos) {
+      // Print out quantization
+      client.hgetall(key,  function (err, res) {
+        time.push(key);
+        var  pose = "";
+        for (var str in res) {
+          if(str.substring(0,8) == "CallHeat" && str.substring(9,13) == "lowt") {
+            pose = res[str];
+          }
+
+          if(str.substring(0,8) == "CallHeat" && str.substring(9,14) == "value") {
+            appendArray(Number(pose), res[str], pos, keys.length);
+            pose = ""; 
+          }
+        }
+
+        if (pos == (keys.length - 1)) { 
+          console.log("times       " + time);
+          for(var pose in types) {
+            console.log(pose + " - " + (2* pose) + "             " +  multi[pose]);
+          }
+          shutDown();
+        }
+      }); 
+    });
+  });
+}
+
+// Straight up prints the data for a given time
+function printData(times) {
+  client.keys("*", function (err, keys) {
+    keys.sort(function(a,b){return a - b});
+    times.forEach(function(time , pos) {
+      client.hgetall(keys[time],  function (err, res) {
+      console.log(keys[time]);
+      console.log(res);
+      if(pos == times.length -1) shutDown();
+      });
+    });
+  });
+}
+
+var arr = [10, 12];
+getStat("Net", "rbytes64", 0, 4000, "Net");
