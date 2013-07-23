507a7fc1287bf600a3e7e032a7601bb418d5f5f1
George Wong <georgewong93@gmail.com>
2013-07-23 17:34:04 -0400

    c++ server | dtrace idle bug fix

--- server/scripts.h
+++ server/scripts.h
@@ -8,7 +8,7 @@
  *  CREATED:  17 JULY 2013
  *  UPDATED:  23 JULY 2013
  */
-
+    
 #define MEM_NUM 7
 #define NET_NUM 4
 #define DISK_NUM 6
@@ -17,29 +17,29 @@
 namespace S {
 
   namespace MEM {
-    int number = MEM_NUM;
+    int number = MEM_NUM; 
     std::string module[ MEM_NUM ] = { "unix", "unix", "unix", "memory_cap", "memory_cap", "memory_cap", "memory_cap" };
     std::string name[ MEM_NUM ] = { "system_pages", "system_pages", "system_pages", "NULL", "NULL", "NULL", "NULL" };
-    std::string statistic[ MEM_NUM ] = { "freemem", "physmem", "pp_kernel", "physcap", "rss", "swap", "swapcap" };
-  }
+    std::string statistic[ MEM_NUM ] = { "freemem", "physmem", "pp_kernel", "physcap", "rss", "swap", "swapcap" };      
+  } 
 
   namespace NET {
     int number = NET_NUM;
     std::string module[ NET_NUM ] = { "link", "link", "link", "link" };
     std::string name[ NET_NUM ] = { "net0", "net0", "net0", "net0" };
-    std::string statistic[ NET_NUM ] = { "obytes64", "rbytes64", "opackets", "ipackets" };
+    std::string statistic[ NET_NUM ] = { "obytes64", "rbytes64", "opackets", "ipackets" };      
   }
 
   namespace DISK {
     int number = DISK_NUM;
     std::string module[ MEM_NUM ] = { "sd", "sd", "sd", "sd", "sd", "sd" };
     std::string name[ MEM_NUM ] = { "NULL", "NULL", "NULL", "NULL", "NULL", "NULL" };
-    std::string statistic[ MEM_NUM ] = { "nread", "nwritten", "reads", "writes", "wtime", "wlentime" };
+    std::string statistic[ MEM_NUM ] = { "nread", "nwritten", "reads", "writes", "wtime", "wlentime" };      
   }
-
+  
   namespace DTRACE {
     int number = DTRACE_NUM;
-
+  
     std::string dtrace[DTRACE_NUM] = {
       (std::string)"profile:::profile-4999\n{\n@[0,cpu] = count();\n}",
       (std::string)"profile:::profile-4999\n{\n@[1,cpu,execname,pid] = count();\n}",
@@ -51,5 +51,6 @@ namespace S {
     std::string proc = "profile:::profile-4999\n{\n@P[pid,execname,cpu] = count();\n}";
     std::string ticks = "profile:::tick-4999\n{\n@P = count();\n}";
   }
+  
+} 
 
-}
