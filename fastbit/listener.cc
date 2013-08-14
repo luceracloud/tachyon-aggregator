76b186aeff4cd35d9e56d84aa15a86c002c8f517
Super-User <root@listener.local>
2013-08-14 14:55:56 -0400

    cleans tables every hour (memory), help page

--- fastbit/listener.cc
+++ fastbit/listener.cc
@@ -7,9 +7,8 @@
  *  directories by IP-zonename.
  *
  *  CREATED:  8 AUG 2013
- *  EDITED:   9 AUG 2013
+ *  EDITED:  14 AUG 2013
  */
-
 
 #include <zmq.hpp>
 #include <iostream>
@@ -21,11 +20,9 @@
 #include "zmq.hpp"
 #include "fastbit.hpp"
 
-
+/* Forward declaration */
+void usage ();
 void sig_handler (int s);
-
-bool get_table (std::string zonename);
-
 ibis::tablex *find_table (std::string zonename,
                           std::map <size_t, std::string> *Indices,
                           std::map <std::string, ibis::tablex *> *Tables);
@@ -37,7 +34,7 @@ time_t save_rate = 60;
 time_t last_save = 0;
 
 /*
- * main loop
+ *  Entry point 
  */
 int main (int argc, char **argv) {
 
@@ -45,8 +42,8 @@ int main (int argc, char **argv) {
   std::cout << "\n Collector program starting.\n" << std::endl;
   UTIL::clear();
 
+  std::string IP = "172.20.0.85";
   std::string PORT = "7211";
-
 
   /* Parse command line */
   for (size_t i=0; i<argc; i++) {
@@ -64,12 +61,16 @@ int main (int argc, char **argv) {
       UTIL::cyan();
       std::cout << " . printing usage information" << std::endl;
       UTIL::clear();
-     // usage();
+      usage();
       return 1;
     }
     if (!strcmp (argv[i], "-p") || !strcmp (argv[i], "-P")) {
       PORT = argv[i+1];
       std::cout << "\033[00;36m . using port " << PORT << "\033[00m\n"; 
+    }
+    if (!strcmp (argv[i], "-a") || !strcmp (argv[i], "-A")) {
+      IP = argv[i+1];
+      std::cout << "\033[00;36m . using IP " << IP << "\033[00m\n";
     }
   }
 
@@ -94,7 +95,6 @@ int main (int argc, char **argv) {
   /* Set up sockets to listen to servers */
   zmq::socket_t listen_socket (context, ZMQ_SUB);
   std::ostringstream CON;
-  std::string IP = "172.20.0.85";
   CON << "tcp://" << IP << ":" << PORT;
   
   listen_socket.connect(CON.str().c_str()); 
@@ -113,40 +113,47 @@ int main (int argc, char **argv) {
       UTIL::clear();
     }
 
-    listen_socket.recv (&msg);
-    pckt.ParseFromArray (msg.data(), msg.size());
+    /* Wait unless (e.g.) we interrupt */
+    try {
+      listen_socket.recv (&msg);
 
-    if (VERBOSE) {
-      std::cout << "Packet number " << packet_counter << std::endl;
-      std::cout << "Size of previous packet: " << msg.size() << std::endl;
-    }
+      pckt.ParseFromArray (msg.data(), msg.size());
 
-    /* Select proper table */
-    if (VERBOSE) std::cout << "Zone name: " << pckt.name() << std::endl;
-    ibis::tablex *tbl = find_table (pckt.name(), &ZoneIndices, &ZoneTables);
+      if (VERBOSE) {
+        std::cout << "Packet number " << packet_counter << std::endl;
+        std::cout << "Size of previous packet: " << msg.size() << std::endl;
+      }
 
-    /* Read into table */
-    bool is_gz = false;
-    if (pckt.name() == "global") {
-      if (VERBOSE) std::cout << "Zone is global, using global insertion functions." << std::endl;
-      is_gz = true;
-    }
+      /* Select proper table */
+      if (VERBOSE) std::cout << "Zone name: " << pckt.name() << std::endl;
+      ibis::tablex *tbl = find_table (pckt.name(), &ZoneIndices, &ZoneTables);
 
-    new_line.str("");
-    new_line.clear();
-    FASTBIT::read_gen (&pckt, &new_line, is_gz);
-    FASTBIT::read_mem (&pckt, &new_line, is_gz);
-    FASTBIT::read_cpu (&pckt, &new_line, is_gz);
-    FASTBIT::read_disk (&pckt, &new_line, is_gz);
-    FASTBIT::read_net (&pckt, &new_line, is_gz);
-    FASTBIT::read_proc (&pckt, &new_line, is_gz);
+      /* Read into table */
+      bool is_gz = false;
+      if (pckt.name() == "global") {
+        if (VERBOSE) std::cout << "Zone is global, using global insertion functions." << std::endl;
+        is_gz = true;
+      }
 
-    tbl->appendRow ((const char *)new_line.str().c_str());
+      new_line.str("");
+      new_line.clear();
+      FASTBIT::read_gen (&pckt, &new_line, is_gz);
+      FASTBIT::read_mem (&pckt, &new_line, is_gz);
+      FASTBIT::read_cpu (&pckt, &new_line, is_gz);
+      FASTBIT::read_disk (&pckt, &new_line, is_gz);
+      FASTBIT::read_net (&pckt, &new_line, is_gz);
+      FASTBIT::read_proc (&pckt, &new_line, is_gz);
 
-    if (VERBOSE) {
-      std::cout << "Table ptr is " << tbl << std::endl;
-      std::cout << "Line returned for this table is: \n";
-      std::cout << new_line.str() << std::endl;
+      tbl->appendRow ((const char *)new_line.str().c_str());
+
+      if (VERBOSE) {
+        std::cout << "Table ptr is " << tbl << std::endl;
+        std::cout << "Line returned for this table is: \n";
+        std::cout << new_line.str() << std::endl;
+      }
+    } catch (zmq::error_t e) {
+      UTIL::yellow();
+      std::cout << "ZMQ listening error:\n" << e.what() << std::endl;
     }
 
     /* Write to database */
@@ -165,6 +172,15 @@ int main (int argc, char **argv) {
         std::cout << "Saved table for " << NAME.str() << std::endl;
         UTIL::clear();
       }
+      /* If we don't clear here, we could end up
+       * with massive overhead, attempting to 
+       * access every zone ever created. Admittedly
+       * this adds a bit of overhead each save, but
+       * it has the potential to save us in the long
+       * run.
+       */
+      ZoneIndices.clear();
+      ZoneTables.clear();
     }
   }
 
@@ -222,7 +238,6 @@ ibis::tablex *find_table (std::string zonename,
   return Tables->at (zonename); 
 }
 
-
 /* Print out message on SIG and
  * also ensures that the table
  * is saved.
@@ -232,5 +247,19 @@ void sig_handler (int s) {
   std::cout << "\n\nReceived signal interrupt\n" << std::endl;
   UTIL::clear();
   run = false;
+}
+
+/*
+ *  Prints usage information to user
+ */ 
+void usage () {
+  UTIL::white();
+  std::cout << "\nusage:  listener [-h] [-v] [-p NUMBER] [-a ADDR]"; 
+  std::cout << "\n    -h         prints this help/usage page";
+  std::cout << "\n    -v         run in verbose mode (print all queries and ZMQ packets)";
+  std::cout << "\n    -p PORT    use port PORT";
+  std::cout << "\n    -a ADDR    listen to IP address ADDR";
+  std::cout << "\n\n";
+  UTIL::clear();
 }
 
