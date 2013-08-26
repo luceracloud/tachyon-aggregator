d32f9fc0320dac41e415d4beb333f963951fc4ef
George Wong <george@somewhereville.com>
2013-08-26 12:59:59 -0400

    Updates to gui

--- web-server/server.js
+++ web-server/server.js
@@ -1,6 +1,4 @@
 /*
- *
- *
  *  server.js
  *
  *
@@ -20,6 +18,43 @@ var io = require('socket.io').listen(server);
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
+io.set('log level', 1);
 
-server.listen(80);
+server.listen(8000);
 
+
+
+
+/* Handling zmq/protobuf parsing */
+var machine_list = [];
+machine_list.push("mac I");
+machine_list.push("macc II");
+machine_list.push("maccc III");
+console.log(machine_list);
+
+
+/* Handling http requests */
+io.sockets.on('connection', function(socket) { 
+
+  /* Send message with list of available machines
+  socket.emit("message", {"type" : "cmd", "cmd" : "dfUse"});
+
+  console.log("Connection");
+
+  /* Handle new message events */
+  socket.on( 'message', function(message, flags) {
+
+    // Init / ident message
+    if (message["type"]=="init") {
+      console.log(machine_list);
+      socket.emit("message", {"type" : "machine_list", "list" : machine_list})
+    } else if (message["type"]="request") {
+      console.log(message);
+
+    }
+
+
+  });
+
+
+});
