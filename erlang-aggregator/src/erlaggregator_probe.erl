f22e71d25758b21c4bb9089cf9505784ab11920b
Heinz N. Gies <heinz@licenser.net>
2013-09-20 12:22:24 +0000

    Added a lot of data to write to Kairos.

--- erlang-aggregator/src/erlaggregator_probe.erl
+++ erlang-aggregator/src/erlaggregator_probe.erl
@@ -9,6 +9,7 @@
 -module(erlaggregator_probe).
 
 -behaviour(gen_server).
+-include("packet_pb.hrl").
 
 %% API
 -export([start_link/1, poll/1, stop/1]).
@@ -19,7 +20,10 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {sock, ip}).
+-define(DB_SERVER, "127.0.0.1").
+-define(DB_PORT, 4242).
+
+-record(state, {tcp, sock, ip, db}).
 
 %%%===================================================================
 %%% API
@@ -60,8 +64,9 @@ init([IP]) ->
     {ok, Ctx} = erlaggregator_server:ctx(),
     {ok, Sock} = erlzmq:socket(Ctx, pull),
     erlzmq:connect(Sock, "tcp://" ++ IP ++ ":7211"),
+    {ok, DB} = gen_tcp:connect(?DB_SERVER, ?DB_PORT, [{packet, line}]),
     poll(IP),
-    {ok, #state{sock = Sock, ip = IP}}.
+    {ok, #state{sock = Sock, ip = IP, db=DB}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -94,10 +99,30 @@ handle_call(_Request, _From, State) ->
 %%--------------------------------------------------------------------
 handle_cast(poll, State =
                 #state{sock = Sock,
+                       db = DB,
                        ip = IP}) ->
     {ok, Data} = erlzmq:recv(Sock),
-    Size = byte_size(Data),
-    erlaggregator_server:inc(Size),
+    Packet = packet_pb:decode_packet(Data),
+    Host = Packet#packet.name,
+    Time = Packet#packet.time,
+    send(DB, Time, Host, "machines.threads", Packet#packet.threads),
+    send(DB, Time, Host, "machines.processes", Packet#packet.processes),
+    [gen_tcp:send(DB,
+       io_lib:format("put machines.cpu ~p ~p host=~s cpu=~p~n",
+                     [Time, CPU#packet_cpu.usage, Host, CPU#packet_cpu.core]))
+       || CPU <- Packet#packet.cpu],
+    [Mem | _] = Packet#packet.mem,
+    send(DB, Time, Host, "machines.rss", Mem#packet_mem.rss), 
+    send(DB, Time, Host, "machines.swap", Mem#packet_mem.swap), 
+    CPUData = [
+       io_lib:format("put machines.cpu ~p ~p host=~s cpu=~p~n",
+                     [Time, CPU#packet_cpu.usage, Host, CPU#packet_cpu.core])
+       || CPU <- Packet#packet.cpu],
+    gen_tcp:send(DB, CPUData),
+    NetData = [fmt_net(Time, Host, Net) || Net <- Packet#packet.net],
+    gen_tcp:send(DB, NetData),
+    DiskData = [fmt_disk(Time, Host, Disk) || Disk <- Packet#packet.disk],
+    gen_tcp:send(DB, DiskData),
     poll(IP),
     {noreply, State};
 
@@ -106,6 +131,7 @@ handle_cast(stop, State) ->
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
+
 
 %%--------------------------------------------------------------------
 %% @private
@@ -149,3 +175,53 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+send(DB, Time, Name, Metric, Value) ->
+    gen_tcp:send(DB,
+      io_lib:format("put ~s ~p ~p host=~s~n",
+                    [Metric, Time, Value, Name])).
+
+fmt_disk(Time, Name, P = #packet_disk{
+  instance = Instance
+}) ->
+  [io_lib:format("put machines.disk.~s ~p ~p host=~s interface=~s~n",
+                  [Metric, Time, def(Value), Name, Instance]) ||
+   {Metric, Value} <- [
+      {"nread", P#packet_disk.nread},
+      {"nwritten", P#packet_disk.nwritten},
+      {"reads", P#packet_disk.reads},
+      {"writes", P#packet_disk.writes},
+      {"rtime", P#packet_disk.rtime},
+      {"wtime", P#packet_disk.wtime},
+      {"rlentime", P#packet_disk.rlentime},
+      {"wlentime", P#packet_disk.wlentime},
+      {"harderror", P#packet_disk.harderror},
+      {"softerror", P#packet_disk.softerror},
+      {"tranerror", P#packet_disk.tranerror}]].
+
+def(undefined) -> 0;
+def(V) -> V.
+
+fmt_net(Time, Name, #packet_net{
+    obytes64 = OBytes,
+    rbytes64 = RBytes,
+    opackets = OPkgs,
+    ipackets = IPkgs,
+    oerrors = OErrors,
+    ierrors = IErrors,
+    instance = I0
+  }) ->
+  IFace = case I0 of
+    undefined ->
+      "vm-local";
+    _ ->
+      I0
+  end,
+  [io_lib:format("put machines.net.~s ~p ~p host=~s interface=~s~n",
+                  [Metric, Time, Value, Name, IFace]) ||
+   {Metric, Value} <- [
+      {"obytes", OBytes},
+      {"rbytes", RBytes},
+      {"opkgs", OPkgs},
+      {"ipkgs", IPkgs},
+      {"oerrors", OErrors},
+      {"ierrors", IErrors}]].
