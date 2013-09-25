a169a717c00a14c5f42358f12aced538689a2395
Heinz N. Gies <heinz@licenser.net>
2013-09-25 20:36:25 +0000

    Fixed typo bug.

--- src/tachyon_app.erl
+++ src/tachyon_app.erl
@@ -10,7 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    tachyone_sup:start_link().
+    tachyon_sup:start_link().
 
 stop(_State) ->
     ok.
