9f840871e4865b36dd4da5a30303839cfd491445
Heinz N. Gies <heinz@licenser.net>
2014-03-26 22:31:55 +0100

    Connect now takes a timeout.

--- apps/tachyon/src/tachyon_kairos.erl
+++ apps/tachyon/src/tachyon_kairos.erl
@@ -15,8 +15,9 @@ put(Metric, Value, Time, Args, K = #kairosdb{db=DB, host=Host, port=Port}) ->
     Metrics = fmt(Metric, Value, Time, Args),
     DB1 = case gen_tcp:send(DB, Metrics) of
               {error, Reason} ->
+                  gen_tcp:close(DB),
                   lager:error("[~s] Socket died with: ~p", [Host, Reason]),
-                  {ok, NewDB} = gen_tcp:connect(Host, Port, [{packet, line}]),
+                  {ok, NewDB} = gen_tcp:connect(Host, Port, [{packet, line}], 100),
                   K#kairosdb{db=NewDB};
               _ ->
                   K
