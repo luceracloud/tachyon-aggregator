7223ee3cc1227d50318ffd8db75f8c88b2baba38
Heinz N. Gies <heinz@licenser.net>
2014-05-27 21:08:47 +0200

    Inlined decoding.

--- apps/tachyon/src/tachyon_kstat_nsq.erl
+++ apps/tachyon/src/tachyon_kstat_nsq.erl
@@ -17,125 +17,182 @@ response(_Msg, State) ->
 error(_Msg, State) ->
     {ok, State}.
 
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          2:32/integer, "ip",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          _Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    put(<<"cloud.host.ip.", Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          7:32/integer, "arcstat",
+          3:32/integer, "zfs",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          _Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    Metric = <<"arc.", Key/binary>>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 4),
+    put(<<"cloud.host.arc.", Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          2:32/integer, "sd",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].", Key/binary>>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 4),
+    put(<<"cloud.host.disk.metrics.", Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          5:32/integer, "sderr",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          11:32/integer, "Hard Errors",
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].errors.hard">>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 1),
+    put(<<"cloud.host.disk.errors.hard">>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          5:32/integer, "sderr",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          11:32/integer, "Soft Errors",
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].errors.soft">>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 1),
+    put(<<"cloud.host.disk.errors.soft">>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          5:32/integer, "sderr",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          16:32/integer, "Transport Errors",
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].errors.transport">>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 1),
+    put(<<"cloud.host.disk.errors.transport">>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          5:32/integer, "sderr",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          27:32/integer, "Predictive Failure Analysis",
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].", "predicted_failures">>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 1),
+    put(<<"cloud.host.disk.errors.predicted_failures">>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          5:32/integer, "sderr",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          15:32/integer, "Illegal Request",
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    Metric = <<"sd[", ID/binary, "].", "illegal_requests">>,
+    tachyon_guard:put(Host, SnapTime, Metric, V, 1),
+    put(<<"cloud.host.disk.errors.illegal">>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
+
+%%
+%% CPU Load
+%%
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, _Name:_NameSize/binary,
+          7:32/integer, "cpu_stat",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    ID = list_to_binary(integer_to_list(Instance)),
+    tachyon_guard:put(Host, SnapTime, <<"cpu[", ID/binary, "].", Key/binary>>, V, 4),
+    put(<<"cloud.host.cpu.", Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}, {<<"cpu">>, Instance}], State);
+
+%%
+%% IP_NIC_EVENT_QUEUE
+%%
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          18:32/integer, "IP_NIC_EVENT_QUEUE",
+          4:32/integer, "unix",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          _Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    put(<<"cloud.host.ip_nic_event_queue.", Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}], State);
+
+message(<<_HostSize:32/integer, Host:_HostSize/binary,
+          6:32/integer, "global",
+          SnapTime:64/integer,
+          _NameSize:32/integer, Name:_NameSize/binary,
+          4:32/integer, "unix",
+          _ClassSize:32/integer, _Class:_ClassSize/binary,
+          _Instance:64/integer,
+          _KeySize:32/integer, Key:_KeySize/binary,
+          $i, V:64/integer>>, _, State) ->
+    put(<<"cloud.host.cache.", Name/binary, $., Key/binary>>, V, SnapTime,
+        [{<<"host">>, Host}], State);
+
+message(<<_HostSize:32/integer, _Host:_HostSize/binary,
+          6:32/integer, "global",
+          _/binary>>, _, State) ->
+    State;
+
 message(Msg, _, State) ->
     tachyon_mps:provide(),
     case tachyon_kstat_pkg:decode(Msg) of
-        {ok, {_, <<"global">>, _, _, _} = P} ->
-            {ok, do_host(P, State)};
+        {ok, {_, <<"global">>, _, _, _}} ->
+            State;
         {ok, P} ->
             {ok, do_zone(P, State)};
         E ->
             lager:error("[msg] ~p", [E]),
             {ok, State}
     end.
-
-
-do_host(Msg, State) ->
-    case Msg of
-        {Host, _, SnapTime, {<<"ip">>, _Instance, _Name, _Class}, {Key, V}} ->
-            tachyon_guard:put(Host, SnapTime, <<"ip.", Key/binary>>, V, 4),
-            put(<<"cloud.host.ip.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime, {<<"zfs">>, _Instance, <<"arcstat">>, _Class}, {Key, V}} ->
-            Metric = <<"arc.", Key/binary>>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 4),
-            put(<<"cloud.host.arc.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime,
-         {<<"sd">>, Instance, _Name, _Class}, {Key, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].", Key/binary>>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 4),
-            put(<<"cloud.host.disk.metrics.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        {Host, _, SnapTime,
-         {<<"sderr">>, Instance, _Name, _Class}, {<<"Hard Errors">>, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].errors.hard">>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 1),
-            put(<<"cloud.host.disk.errors.hard">>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        {Host, _, SnapTime,
-         {<<"sderr">>, Instance, _Name, _Class}, {<<"Soft Errors">>, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].errors.hard">>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 1),
-            put(<<"cloud.host.disk.errors.soft">>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        {Host, _, SnapTime,
-         {<<"sderr">>, Instance, _Name, _Class}, {<<"Transport Errors">>, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].errors.transport">>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 1),
-            put(<<"cloud.host.disk.errors.transport">>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        {Host, _, SnapTime,
-         {<<"sderr">>, Instance, _Name, _Class}, {<<"Illegal Request">>, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].", "illegal_requests">>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 1),
-            put(<<"cloud.host.disk.errors.illegal">>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        {Host, _, SnapTime,
-         {<<"sderr">>, Instance, _Name, _Class}, {<<"Predictive Failure Analysis">>, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            Metric = <<"sd[", ID/binary, "].", "predicted_failures">>,
-            tachyon_guard:put(Host, SnapTime, Metric, V, 1),
-            put(<<"cloud.host.disk.errors.predicted_failures">>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"disk">>, Instance}], State);
-
-        %% CPU Load
-        {Host, _, SnapTime,
-         {<<"cpu_stat">>, Instance, _Name, _Class}, {Key, V}} ->
-            ID = list_to_binary(integer_to_list(Instance)),
-            tachyon_guard:put(Host, SnapTime, <<"cpu[", ID/binary, "].", Key/binary>>, V, 4),
-            put(<<"cloud.host.cpu.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}, {<<"cpu">>, Instance}], State);
-
-        %% IP_NIC_EVENT_QUEUE
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"vnd_str_cache">>, _Class}, {Key, V}} ->
-
-            put(<<"cloud.host.cache.streams.vnd.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"dld_str_cache">>, _Class}, {Key, V}} ->
-
-            put(<<"cloud.host.cache.streams.vnd.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"udp_conn_cache">>, _Class}, {Key, V}} ->
-            put(<<"cloud.host.cache.connections.udp.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"tcp_conn_cache">>, _Class}, {Key, V}} ->
-            put(<<"cloud.host.cache.connections.tcp.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"socket_cache">>, _Class}, {Key, V}} ->
-            put(<<"cloud.host.cache.socket.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-
-        %% Netork Caches
-        {Host, _, SnapTime,
-         {<<"unix">>, _Instance, <<"IP_NIC_EVENT_QUEUE">>, _Class}, {Key, V}} ->
-            put(<<"cloud.host.ip_nic_event_queue.", Key/binary>>, V, SnapTime,
-                [{<<"host">>, Host}], State);
-        _ ->
-            State
-    end.
-
 
 do_zone(Msg, State) ->
     case Msg of
