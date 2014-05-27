c5f0bf24728e52ebf07352c30ca4696d0b7b2f98
Heinz N. Gies <heinz@licenser.net>
2014-05-27 05:14:36 +0200

    Some improvements for parsing kstat messages.

--- apps/tachyon/src/tachyon_kstat_pkg.erl
+++ apps/tachyon/src/tachyon_kstat_pkg.erl
@@ -10,22 +10,21 @@ decode(<<_HostSize:32/integer, Host:_HostSize/binary,
          _ClassSize:32/integer, Class:_ClassSize/binary,
          Instance:64/integer,
          _KeySize:32/integer, Key:_KeySize/binary,
-         Data/binary>>) ->
-    case decode_data(Data) of
-        {ok, V} ->
-            %% {ok, {Host, Zone, trunc(Time/1000000000), {Module, Instance, Name, Class},
-            {ok, {Host, Zone, Time, {Module, Instance, Name, Class},
-                  {Key, V}}};
-        _ ->
-            {error, unknown_data, Data}
-    end;
+         $i, Integer:64/integer>>) ->
+    {ok, {Host, Zone, Time, {Module, Instance, Name, Class},
+          {Key, Integer}}};
+
+decode(<<_HostSize:32/integer, Host:_HostSize/binary,
+         _ZoneSize:32/integer, Zone:_ZoneSize/binary,
+         Time:64/integer,
+         _NameSize:32/integer, Name:_NameSize/binary,
+         _ModuleSize:32/integer, Module:_ModuleSize/binary,
+         _ClassSize:32/integer, Class:_ClassSize/binary,
+         Instance:64/integer,
+         _KeySize:32/integer, Key:_KeySize/binary,
+         $s, String/binary>>) ->
+    {ok, {Host, Zone, Time, {Module, Instance, Name, Class},
+          {Key, String}}};
 
 decode(B) ->
     {error, unknown_package, B}.
-
-decode_data(<<$i, Integer:64/integer>>) ->
-    {ok, Integer};
-decode_data(<<$s, String/binary>>) ->
-    {ok, String};
-decode_data(_) ->
-    error.
