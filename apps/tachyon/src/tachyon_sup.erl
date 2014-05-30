52f2bebe74f30a40b84520a7b4e3f9e4d6481d59
Heinz N. Gies <heinz@licenser.net>
2014-05-30 09:38:16 +0200

    Removed guard sup.

--- apps/tachyon/src/tachyon_sup.erl
+++ apps/tachyon/src/tachyon_sup.erl
@@ -35,7 +35,6 @@ init([]) ->
           [
            ?CHILD(tachyon_mps, worker),
            ?CHILD(tachyon_metric_sup, supervisor),
-           ?CHILD(tachyon_guard_sup, supervisor),
            ?CHILD(tproc, worker),
            ?CHILD(tachyon_scheduler, worker),
            ?CHILD(tachyon_server, worker)]}}.
